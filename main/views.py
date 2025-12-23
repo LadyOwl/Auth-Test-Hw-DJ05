7e7690288d089227f65315334dfa2556ae290afb
Юлия (Джулия) <sixquestions666@gmail.com>
2025-12-23 15:19:42 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# main/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
