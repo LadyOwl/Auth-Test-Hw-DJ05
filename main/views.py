bdd5f8a0f90167eb2dd20e32fa0293477effa761
Юлия (Джулия) <sixquestions666@gmail.com>
2025-12-29 14:12:24 +0300

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,22 +1,22 @@
 # main/views.py
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from .forms import CustomUserCreationForm  # импортируем новую форму
 
 def index(request):
     return render(request, 'index.html')
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Account created successfully! Please log in.')
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'register.html', {'form': form})
 
 class ProfileView(LoginRequiredMixin, TemplateView):
