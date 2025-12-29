cab193678945c54d5a1949f23cdf46797e35613c
Юлия (Джулия) <sixquestions666@gmail.com>
2025-12-29 14:08:28 +0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
+# main/models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'  # Используем email для входа
+    REQUIRED_FIELDS = ['username']  # username всё ещё требуется (но можно убрать, если нужно)
+
+    def __str__(self):
+        return self.email
+
+
 from django.db import models
 
 # Create your models here.
