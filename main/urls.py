07a1bfb681b35e7389ccdde0c12316de6cb0c2a6
Юлия (Джулия) <sixquestions666@gmail.com>
2025-12-29 13:52:25 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 # main/urls.py
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('profile/', views.profile, name='profile'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
     path('login/', LoginView.as_view(template_name='login.html', redirect_authenticated_user=True), name='login'),
-    path('logout/', views.logout_view, name='logout'),
+    path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
+    path('register/', views.register, name='register'),
 ]
